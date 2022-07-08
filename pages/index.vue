4c420e155d266a21e7e0a6e28de814c5020ab96d
ata parvin ghods <ataparvinghods@gmail.com>
2022-07-08 17:58:04 +0430

    Added hero bar

--- pages/index.vue
+++ pages/index.vue
@@ -26,8 +26,62 @@
       </section>
       <!--Hero-->
       <!--Hero Bar-->
-      <section id="hero-bar" class="border shadow mx-15">
-        <div class="bar-container white pa-15">d</div>
+      <section id="hero-bar" class="border shadow mx-15 mb-15">
+        <div class="bar-container white px-15 py-7">
+          <div class="facilities adjust-items-with-height">
+            <p class="title">Facilities</p>
+            <div class="facility-items">
+              <div
+                class="d-flex flex-column justify-center align-center facility-card"
+                v-for="(item, index) in facilityItems"
+                :key="index"
+              >
+                <v-btn outlined icon :color="item.color">
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-btn>
+                <p class="mb-0">{{ item.title }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="total-customers adjust-items-with-height">
+            <div>
+              <p class="title mb-0">Total Customers</p>
+              <p class="caption">And counting on...</p>
+            </div>
+            <p class="display-1 mb-0">+3,323,123</p>
+          </div>
+          <div class="online-services adjust-items-with-height">
+            <div>
+              <p class="title mb-0">Online Services</p>
+              <p class="caption">Talk to our online advisors!</p>
+            </div>
+            <div class="advisors">
+              <v-tooltip
+                bottom
+                v-for="(item, index) in onlineAdvisors"
+                :key="index"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-avatar
+                    color="primary"
+                    size="56"
+                    class="mr-5"
+                    v-bind="attrs"
+                    v-on="on"
+                    style="cursor: pointer"
+                  >
+                    <img
+                      :src="require(`/assets/images/${item.img}`)"
+                      :alt="item.name"
+                    />
+                  </v-avatar>
+                </template>
+                <span>{{ item.name }}</span>
+              </v-tooltip>
+              ...
+            </div>
+          </div>
+        </div>
       </section>
       <!--Hero Bar-->
     </div>
@@ -45,6 +99,55 @@ export default {
           description:
             'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias quam sint tempore vel, voluptatibus.',
           price: 99,
+        },
+      ],
+      facilityItems: [
+        {
+          icon: 'mdi-sun-snowflake',
+          title: 'Cooler',
+          color: 'purple',
+        },
+        {
+          icon: 'mdi-face-agent',
+          title: '24h Service',
+          color: 'orange',
+        },
+        {
+          icon: 'mdi-food',
+          title: 'Free Food',
+          color: 'pink',
+        },
+        {
+          icon: 'mdi-parking',
+          title: 'Parking',
+          color: 'cyan',
+        },
+        {
+          icon: 'mdi-pool',
+          title: 'Pool',
+          color: 'green',
+        },
+      ],
+      onlineAdvisors: [
+        {
+          name: 'Bill Gates',
+          img: 'person1.jpg',
+        },
+        {
+          name: 'Morgan Freeman',
+          img: 'person2.jpg',
+        },
+        {
+          name: 'Deepika Padukone',
+          img: 'person3.jpg',
+        },
+        {
+          name: 'Megan Fox',
+          img: 'person4.jpg',
+        },
+        {
+          name: 'Dwayne Johnson',
+          img: 'person5.jpg',
         },
       ],
     }
@@ -90,5 +193,27 @@ export default {
 }
 #hero-bar {
   transform: translateY(-50%);
+
+  .bar-container {
+    display: grid;
+    grid-template-columns: 40% 20% 40%;
+
+    .adjust-items-with-height {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    .facilities {
+      .facility-items {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      .facility-card {
+        margin-right: 30px;
+      }
+    }
+  }
 }
 </style>
